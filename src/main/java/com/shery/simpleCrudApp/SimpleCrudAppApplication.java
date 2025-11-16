9006fb23c6e0268b0e7e6ef1704dc5fa9b5ddffd
webwithsumit <sumit2410kushwaha@gmail.com>
2025-11-16 16:57:45 +0530

    second commit

--- src/main/java/com/shery/simpleCrudApp/SimpleCrudAppApplication.java
+++ src/main/java/com/shery/simpleCrudApp/SimpleCrudAppApplication.java
@@ -2,7 +2,6 @@ package com.shery.simpleCrudApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SimpleCrudAppApplication {
